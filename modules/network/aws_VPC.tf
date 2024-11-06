6068e43da349d0342f1ba7ba7e61ba1a001baa71
George Luis <cloudgeorge50@gmail.com>
2024-11-06 02:34:41 -0300

    Add. recurso de VPC

--- modules/network/aws_VPC.tf
+++ modules/network/aws_VPC.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "vpc_prd" {
     cidr_block = var.cidr_block 
-    enable_dns_hostnames = true
+    enable_dns_hostnames = true #Necessário para instâncias públicas acessíveis via DNS/ Habilita a resolução de DNS interno para os recursos dentro da VPC. Ou seja, permite que instâncias dentro da VPC possam resolver nomes de domínio DNS internos.
+    enable_dns_support   = true  # Garante a resolução de DNS/
     tags = var.vpc_tags
   
 }
